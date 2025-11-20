d87469efa20b8a875ea979cfcef588271e5ba265
Sahil Sahu <sahilsahu731709@gmail.com>
2025-11-20 19:43:01 +0530

    setup basic frontend

---
+++ client/src/app/test/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div>
+       hey there
+       <h1 className='text-4xl text-green-300 font-bold'>Working or not</h1>
+    </div>
+  )
+}
+
+export default Test
